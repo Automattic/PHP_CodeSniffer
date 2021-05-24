a6ac2aa89cab717d9220e2176540d8a4d95f7bfc
Brad Jorsch <anomie@users.sourceforge.net>
2021-05-24 13:35:41 -0400

    Allow the filter to return LocalFile objects to FileList
    This gets us most of the way to fixing #2551. Now it's just a matter of
    writing a filter that updates the Config and/or Ruleset as appropriate
    while processing the directory tree.
    
    Since this change to `Filter::current()` may break expectations of
    existing filters, a filter must opt in to this change by overriding the
    protected `$this->produceLocalFileObjects` to true.

--- src/Files/FileList.php
+++ src/Files/FileList.php
@@ -94,7 +94,11 @@ class FileList implements Iterator, Countable
                 $iterator = new RecursiveIteratorIterator($filter);
 
                 foreach ($iterator as $file) {
-                    $this->files[$file->getPathname()] = null;
+                    if ($file instanceof LocalFile) {
+                        $this->files[$file->getFilename()] = $file;
+                    } else {
+                        $this->files[$file->getPathname()] = null;
+                    }
                 }
             } else {
                 $this->addFile($path);
@@ -134,12 +138,27 @@ class FileList implements Iterator, Countable
         $iterator = new RecursiveIteratorIterator($filter);
 
         foreach ($iterator as $path) {
-            if (array_key_exists($path, $this->files) === true) {
+            if ($path instanceof LocalFile) {
+                $filename = $path->getFilename();
+            } else {
+                $filename = $path;
+            }
+
+            if (array_key_exists($filename, $this->files) === true) {
                 // The path has already been added.
                 continue;
             }
 
-            $this->files[$path] = $file;
+            if ($path instanceof LocalFile) {
+                if ($file !== null) {
+                    $this->files[$filename] = $file;
+                } else {
+                    $this->files[$filename] = $path;
+                }
+            } else {
+                $this->files[$filename] = $file;
+            }
+
             $this->numFiles++;
         }
     }
